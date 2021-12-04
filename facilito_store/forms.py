bc547cf0ceee68b2b800c581b27aebb726cd5bcc
Felipe Contreras <contreras.felipe905@gmail.com>
2021-12-04 13:34:53 -0300

    changes1

--- facilito_store/forms.py
+++ facilito_store/forms.py
@@ -1,6 +1,8 @@
 
 from django import forms
 
+from django.contrib.auth.models import User
+
 class RegisterForm(forms.Form):
     username = forms.CharField(required=True, min_length=4, max_length=50,
                                 widget=forms.TextInput(attrs={
@@ -17,3 +19,37 @@ class RegisterForm(forms.Form):
                                     widget=forms.PasswordInput(attrs={
                                         'class': 'form-control'
                                     }))
+
+    password2 = forms.CharField(label='Confirmar password',
+                                required=True,
+                                widget=forms.PasswordInput(attrs={
+                                    'class': 'form-control'
+                                }))
+    def clean_username(self):
+        username =  self.cleaned_data.get('username')
+
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('el username ya se encuentra en uso')
+
+        return username 
+
+    def clean_email(self):
+        email =  self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('el email ya se encuentra en uso')
+
+        return email                                    
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if cleaned_data.get('password2') != cleaned_data.get('password'):
+            self.add_error('password2', 'el password no coincide')
+
+    def save(self):
+        return User.objects.create_user(
+            self.cleaned_data.get('username'),
+            self.cleaned_data.get('email'),
+            self.cleaned_data.get('password'),
+        )
