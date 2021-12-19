94ce09072f686b28a8a00475e4f7cdc3aae2ce63
felipetabi <contreras.felipe905@gmail.com>
2021-12-18 22:37:18 -0300

    slug

--- facilito_store/settings.py
+++ facilito_store/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'facilito_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates',
+                os.path.join(BASE_DIR, 'products', 'templates', 'products'),
+                ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
